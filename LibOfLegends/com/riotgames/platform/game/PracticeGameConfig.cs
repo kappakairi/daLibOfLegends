d53177b43057d00504f1958327ad825341ba6290
David Santos <kappakairi@gmail.com>
2012-06-28 22:48:45 -0700

    PracticeGameConfig and GameMap data class
    Added the PracticeGameConfig and GameMap data classes to interface with
    RPC calls to server.

---
+++ LibOfLegends/com/riotgames/platform/game/PracticeGameConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using com.riotgames.platform.gameclient.domain;
+using com.riotgames.platform.game.map;
+
+namespace com.riotgames.platform.game
+{
+    public class PracticeGameConfig : AbstractDomainObject
+    {
+        /** \brief Name of Game */
+        public string gameName;
+        /** \brief Password for Game */
+        public string gamePassword;
+        /** \brief Passback URL (usually null) */
+        public string passbackUrl;
+        /** \brief Data Packet for passback (usually null) */
+        public string passbackDataPacket;
+        /** \brief Spectator setting (All, friends, etc) */
+        public string allowSpectators;
+        /** \brief Type of Game Config */
+        public int gameTypeConfig;
+        /** \brief Game mode (Classic, dominion) */
+        public string gameMode;
+        /** \brief Maximum allowed players */
+        public int maxNumPlayers;
+        /** \brief Class type name */
+        public string TypeName;
+        /** \brief Game Map and configuration */
+        public GameMap gameMap;
+    }
+}
---
+++ LibOfLegends/com/riotgames/platform/game/map/GameMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using com.riotgames.platform.gameclient.domain;
+
+namespace com.riotgames.platform.game.map
+{
+    public class GameMap : AbstractDomainObject
+    {
+        /** \brief ID of Map */
+        public int mapId;
+        /** \brief Map Name */
+        public string name;
+        /** \brief Map Display Name */
+        public string displayName;
+        /** \brief Map Description */
+        public string description;
+        /** \brief Total players on map */
+        public int totalPlayers;
+        /** \brief Minimum custom players */
+        public int minCustomPlayers;
+        /** \brief Class type name */
+        public string TypeName;
+    }
+}
