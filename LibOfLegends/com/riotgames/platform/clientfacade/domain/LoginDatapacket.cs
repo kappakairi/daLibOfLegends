b2aeb2a76d1ff7e1579135fd855bff40420f308c
David Santos <kappakairi@gmail.com>
2012-07-04 13:14:54 -0700

    Updated LoginDataPacket object
    Updated LoginDataPacket object with correct values that were missing

--- LibOfLegends/com/riotgames/platform/clientfacade/domain/LoginDatapacket.cs
+++ LibOfLegends/com/riotgames/platform/clientfacade/domain/LoginDatapacket.cs
@@ -18,18 +18,29 @@ namespace com.riotgames.platform.clientfacade.domain
 		public bool clientHeartBeatEnabled;
 		public PlayerStatSummaries playerStatSummaries;
 		public int maxPracticeGameSize;
-		public object reconnectInfo;
+		public object reconnectInfo;                // TODO check this
 		public bool minor;
 		public string platformId;
-		public ArrayCollection gameTypeConfigs;
+		public ArrayCollection gameTypeConfigs;        // TODO fix this
 		public double ipBalance;
 		public ClientSystemStatesNotification clientSystemStates;
-		public object summonerCatalog;
+		public object summonerCatalog;                    // TODO fix this
 		public ArrayCollection languages;
 		public object allSummonerData;
 		public int leaverPenaltyLevel;
 		public object broadcastNotification;
 		public bool matchMakingEnabled;
 		public bool inGhostGame;
+
+        /** \brief Minutes left to earn XP/IP in Custom games */
+        public int customMinutesLeftToday;
+        /** \brief Minutes left to earn XP/IP in CoOpvsAi games */
+        public int coOpVsAiMinutesLeftToday;
+        /** \brief Milliseconds until CoOpvsAi limit is reset */
+        public long coOpVsAiMsecsUntilReset;
+        /** \brief Milliseconds until Custom limit is reset */
+        public long customMsecsUntilReset;
+
+        // TODO look into clientSystemStates and broadcastNotification, LOLNotes shows JSON exists for those
 	}
 }
